753c6a6053440735474748ebde82ceca4dd760a8
Kangsaga <95010758+Kangsaga@users.noreply.github.com>
2021-11-26 10:38:36 +0900

    Add files via upload

---
+++ Exam01이름나이.java
@@ -0,0 +1,18 @@
+
+public class Exam01이름나이 {
+
+	public static void main(String[] args) {
+		//문자열 변수
+		String name = "강범수";
+		System.out.println(name);
+		
+		// 상수 -변하지않는 값을 가진 수 규격,원주율
+		final int age = 24;
+		
+		
+		
+	}
+		
+
+
+}
