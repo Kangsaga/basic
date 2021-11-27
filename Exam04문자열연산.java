b9a367ed2cd4103abc814bb4dcb946f3c98888b1
Kangsaga <95010758+Kangsaga@users.noreply.github.com>
2021-11-27 15:27:43 +0900

    Add files via upload

---
+++ Exam04문자열연산.java
@@ -0,0 +1,18 @@
+
+public class Exam04문자열연산 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//문자열 연산
+		String value = "나는 사과";
+		System.out.println(value + "입니다." + (8 + 9));
+		
+		
+		
+		
+		
+		
+	}
+
+}
