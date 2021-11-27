4c71060d75ad67a38223c77922ce8033e28823a6
Kangsaga <95010758+Kangsaga@users.noreply.github.com>
2021-11-27 15:26:47 +0900

    Add files via upload

---
+++ Exam03자동형변환.java
@@ -0,0 +1,29 @@
+
+public class Exam03자동형변환 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//자동 형변환
+		//정수와 실수를 같이 연산하면 실수타입으로 나온다.
+		//정수보다 실수값이 크기 때문이다.
+		
+		int num1 = 7;
+		int num2 = 3;
+		double num3 = 3.0;
+		
+		System.out.println(num1 + num2);
+		System.out.println(num1 -num2);
+		System.out.println(num1 * num2);
+		System.out.println(num1 / num2);
+		System.out.println(num1 % num2);
+		System.out.println(num1 / num3);
+		System.out.println(num1 % num3);
+		
+		
+		
+		
+		
+	}
+
+}
