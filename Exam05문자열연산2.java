6d9a910db3a45148052e48cbdeabcf27d4e67ed4
Kangsaga <95010758+Kangsaga@users.noreply.github.com>
2021-11-27 15:28:26 +0900

    Add files via upload

---
+++ Exam05문자열연산2.java
@@ -0,0 +1,19 @@
+
+public class Exam05문자열연산2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num1 = 7;
+		int num2 = 3;
+		System.out.println("더하기 결과 : " + (num1 + num2));
+		System.out.println("빼기 결과 : " + (num1 - num2));
+		System.out.println("곱하기결과 : " + (num1 * num2));
+		//System.out.println("나누기 결과 : " + (num1 / (double) num2));
+		System.out.println("나누기 결과 : " + (num1 * 1.0 / num2));
+		
+		
+		
+	}
+
+}
