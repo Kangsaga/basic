51309e95e581a14013ec4be4aea6c462a27af643
Kangsaga <95010758+Kangsaga@users.noreply.github.com>
2021-11-29 13:48:34 +0900

    Add files via upload

---
+++ Exam09복합대입연산.java
@@ -0,0 +1,24 @@
+
+public class Exam09복합대입연산 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//복합대입 연산자
+		int num = 10;
+		
+		//num = num + 2;
+		//num += 2;
+		//num -=5; //num = num -5;
+		//num *= 2; //num *=2;
+		num -= 2 + 3 * 4; //num = num - (2 + 3 * 4);
+		
+		
+		System.out.println(num);
+		
+		
+		
+		
+		
+	}
+
+}
