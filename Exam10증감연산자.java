a48dd888916f03ab51d45fdebcc1d885e27a8587
Kangsaga <95010758+Kangsaga@users.noreply.github.com>
2021-11-29 13:49:04 +0900

    Add files via upload

---
+++ Exam10증감연산자.java
@@ -0,0 +1,21 @@
+
+public class Exam10증감연산자 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//증감연산자
+		int i = 10;
+		
+		//증감연산자가 변수 앞에 붙으면 증감을 먼저하고 다른 일처리를 수행한다
+		//증감연산자가 변수 뒤에 붙으면 다른일처리를 먼저 수행하고 증감을 한다
+		
+		System.out.println(i++);
+		System.out.println(i);
+		
+		
+		
+		
+	}
+
+}
