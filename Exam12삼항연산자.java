d167afc83ee43ec2001bde3648f4302b2c6f5751
Kangsaga <95010758+Kangsaga@users.noreply.github.com>
2021-11-30 11:12:59 +0900

    Add files via upload

---
+++ Exam12삼항연산자.java
@@ -0,0 +1,19 @@
+
+public class Exam12삼항연산자 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i = 3;
+		int j = 10;
+		
+		
+		// 삼항연산자
+		//조건식 ? ture 일때 실행 : fasle 일때 실행 
+		System.out.println(i == j ? "같다 " : "다르다" );
+		
+		
+		
+	}
+
+}
